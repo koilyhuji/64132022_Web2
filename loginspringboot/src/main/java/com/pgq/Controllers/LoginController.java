f61cbec24994d83356d7e7fd30f2572a624cdf1e
bobby-stanoff <hongnguyen210180@gmail.com>
2025-03-17 21:57:40 -0400

    loginSPringmvc

---
+++ loginspringboot/src/main/java/com/pgq/Controllers/LoginController.java
@@ -0,0 +1,57 @@
+package com.pgq.Controllers;
+
+import java.lang.ProcessHandle.Info;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.pgq.Models.User;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+	
+	private List<User> users = new ArrayList<>();
+
+	public LoginController() {
+		users.add(new User("user1", "password1","nguyen van t",2004,"male"));
+        users.add(new User("admin", "adminpass","nguyen van a",2002,"male"));
+	}
+	@GetMapping
+	public String showLoginForm() {
+		return "login";
+	}
+	
+	@PostMapping
+	public String processLogin
+	(@RequestParam String id,
+	 @RequestParam String password, 
+	 ModelMap m) {
+		User user = findUser(id, password);
+
+        
+
+			if(id.equals("666") && password.equals("2233232")) {
+				m.addAttribute("message","Đăng nhập thành công");
+				return "redirect:/home";
+			}
+			else {
+				m.addAttribute("message","Sai thông tin đăng nhập");
+			}
+			return "login";
+	}
+	private User findUser(String id, String password) {
+        for (User user : users) {
+            if (user.getId().equals(id) && user.getPassword().equals(password)) {
+                return user;
+            }
+        }
+        return null;
+    }
+}
---
+++ loginspringboot/src/main/java/com/pgq/Models/User.java
@@ -0,0 +1,61 @@
+package com.pgq.Models;
+
+
+public class User {
+	private String id;
+	private String password;
+	private String name;
+	private int birthYear;
+	private String gender;
+	
+	public User(String id, String password, String name, int birthYear, String gender) {
+		super();
+		this.id = id;
+		this.password = password;
+		this.name = name;
+		this.birthYear = birthYear;
+		this.gender = gender;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getBirthYear() {
+		return birthYear;
+	}
+
+	public void setBirthYear(int birthYear) {
+		this.birthYear = birthYear;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	
+	
+}
