5c83c716f06f9a4231217e4f41e3aebd76db55b9
bobby-stanoff <hongnguyen210180@gmail.com>
2025-03-24 22:25:41 -0400

    push layout

--- TongHopGK/src/main/java/com/pgq/GKApplication.java
+++ TongHopGK/src/main/java/com/pgq/GKApplication.java
@@ -1,13 +1,13 @@
-package com.pgq.template;
+package com.pgq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TemplateApplication {
+public class GKApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TemplateApplication.class, args);
+		SpringApplication.run(GKApplication.class, args);
 	}
 
 }
