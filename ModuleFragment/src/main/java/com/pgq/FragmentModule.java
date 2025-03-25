dc24e8e4159e4ee3e7eb7864db38d8326959ca92
bobby-stanoff <hongnguyen210180@gmail.com>
2025-03-24 21:20:22 -0400

    fragment 1

---
+++ ModuleFragment/src/main/java/com/pgq/FragmentModule.java
@@ -0,0 +1,13 @@
+package com.pgq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FragmentModule {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FragmentModule.class, args);
+	}
+
+}
