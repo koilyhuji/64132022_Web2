de29f9ab25cb8d32d79361e2bf350c687e6c2592
bobby-stanoff <hongnguyen210180@gmail.com>
2025-03-24 21:35:03 -0400

    to chuc bo cuc

---
+++ FragmentToChucBoCuc/src/main/java/com/pgq/ToChucBoCuc.java
@@ -0,0 +1,13 @@
+package com.pgq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToChucBoCuc {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToChucBoCuc.class, args);
+	}
+
+}
