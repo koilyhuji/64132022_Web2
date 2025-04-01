15194106531fa7f9365c2058edb52efd868147ee
bobby-stanoff <hongnguyen210180@gmail.com>
2025-03-31 22:35:07 -0400

    push 930

---
+++ PhamGiaQuynh_thigk/src/main/java/thiGK/ntu64132022/PhamGiaQuynh_thigk/model/Page.java
@@ -0,0 +1,60 @@
+package thiGK.ntu64132022.PhamGiaQuynh_thigk.model;
+
+public class Page {
+    private Long id;
+    private String pageName;
+    private String keyword;
+    private String content;
+    private Long parentPageId;
+
+    public Page() {
+    }
+
+    public Page(Long id, String pageName, String keyword, String content, Long parentPageId) {
+        this.id = id;
+        this.pageName = pageName;
+        this.keyword = keyword;
+        this.content = content;
+        this.parentPageId = parentPageId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPageName() {
+        return pageName;
+    }
+
+    public void setPageName(String pageName) {
+        this.pageName = pageName;
+    }
+
+    public String getKeyword() {
+        return keyword;
+    }
+
+    public void setKeyword(String keyword) {
+        this.keyword = keyword;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Long getParentPageId() {
+        return parentPageId;
+    }
+
+    public void setParentPageId(Long parentPageId) {
+        this.parentPageId = parentPageId;
+    }
+}
