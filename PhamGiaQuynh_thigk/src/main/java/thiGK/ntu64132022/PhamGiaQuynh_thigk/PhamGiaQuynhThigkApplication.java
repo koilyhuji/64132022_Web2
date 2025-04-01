c6a592d9b86fe77024ca8a5d1b8eb9b43ce9ac43
bobby-stanoff <hongnguyen210180@gmail.com>
2025-03-31 22:08:05 -0400

    push github

---
+++ PhamGiaQuynh_thigk/src/main/java/thiGK/ntu64132022/PhamGiaQuynh_thigk/PhamGiaQuynhThigkApplication.java
@@ -0,0 +1,13 @@
+package thiGK.ntu64132022.PhamGiaQuynh_thigk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhamGiaQuynhThigkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhamGiaQuynhThigkApplication.class, args);
+	}
+
+}
